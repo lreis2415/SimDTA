eee27e7fad8a59d559d8b22083b3f6dca1158912
crazyzlj <crazyzlj@gmail.com>
2021-12-18 20:42:31 +0800

    change versions

--- exec/reg_x64.bat
+++ exec/reg_x64.bat
@@ -3,13 +3,13 @@ echo path: %~dp0
 copy %~dp0\TABCTL32.OCX %windir%\SysWOW64\
 copy %~dp0\MSCOMCTL.OCX %windir%\SysWOW64\
 copy %~dp0\TeeChart5.OCX %windir%\SysWOW64\
-copy %~dp0\msflxgrd.OCX %windir%\SysWOW64\
-copy %~dp0\comdlg32.OCX %windir%\SysWOW64\
+copy %~dp0\MSFLXGRD.OCX %windir%\SysWOW64\
+copy %~dp0\COMDLG32.OCX %windir%\SysWOW64\
 @echo Register Libs...
 regsvr32 %windir%\SysWOW64\MSCOMCTL.OCX /s
 regsvr32 %windir%\SysWOW64\TABCTL32.OCX /s
 regsvr32 %windir%\SysWOW64\TeeChart5.OCX /s
-regsvr32 %windir%\SysWOW64\msflxgrd.OCX /s
-regsvr32 %windir%\SysWOW64\comdlg32.OCX /s
+regsvr32 %windir%\SysWOW64\MSFLXGRD.OCX /s
+regsvr32 %windir%\SysWOW64\COMDLG32.OCX /s
 @echo Libs registerd...
 @pause
